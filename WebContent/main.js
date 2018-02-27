7a918baacce76513b7b6397d1f43a45de3e424a1
tgarg6343 <tgarg6343@gmail.com>
2018-02-28 00:25:27 +0530

    second

--- WebContent/main.js
+++ WebContent/main.js
@@ -11,13 +11,53 @@ var req = new Request(url);
 fetch(req)
 	.then(result=>result.json())
 	.then((data)=>{
-		document.getElementById('result').innerHTML=printData(data);
-
+		
+		 document.getElementById('result').innerHTML=printdata(data);
+			
+		
 	})
-	
-	let printData=function(data){
-	var html="";
-	html+=""
-	html+=""
-}
+	}
+
+let printdata=function(data){
+	 var htmlText = '';
+    for ( var i=0;i<20;i++ ) {
+   	 //console.log(data.results[i].title);
+        htmlText += '<div class="div-conatiner">';
+        htmlText += '<div class="col_sm_4">';
+        htmlText += '<p class="p-name"> Author: ' + data.articles[i].author + '</p>';
+        htmlText += '<p class="p-loc">Title: ' + data.articles[i].title + '</p>';
+        htmlText += '<p class="p-desc"> Description: ' + data.articles[i].description + '</p>';
+        htmlText += '<p class="p-created"> Publised at: ' + data.articles[i].publishedAt + '</p>';
+        htmlText += '</div>';
+        
+        htmlText += '<div><img src='+data.articles[i].urlToImage+'></div>';
+        htmlText += '<a href='+data.articles[i].url+'>';
+        htmlText += '<input type="button" value="Visit Page" />';
+        htmlText += '</a>';
+       
+        //htmlText += '<a href="trial.html">';
+        htmlText += '<input type="button" value="Add to Favourites list" />';
+        //htmlText += '</a>';
+        htmlText += '<button id="addfav" onClick=incount() >Add to favourites</button>';
+        //htmlText += '<script type="text/javascript" src="function1.js"></script>';
+        htmlText += '<hr>';
     }
+document.getElementById('result').insertAdjacentHTML('afterend',htmlText);
+}
+function onClick(){
+	callServletByAjax();
+}
+
+function callServletByAjax(){
+	var http=new XMLHttpRequest();
+	http.onreadystatechange=function(){
+		if (http.readyState==4 && http.status==200){
+			document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+		}
+	}
+}
+
+
+
+
+
